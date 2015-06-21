34c0b9bcc56d931389a732474d27791e3954f636
Philippe Joulot <philippe.joulot@laposte.net>
2015-06-21 23:28:08 +0200

    Method to display an image in a canvas pixel by pixel and it allows 4 orientations

--- convert_pixel.js
+++ convert_pixel.js
@@ -6,8 +6,9 @@
 	
 	var json=document.getElementById("result_json");
 	json.value = image_to_json(img);
-	
-	display_image(json.value, img.width, img.height, 2);
+	var canvas=document.getElementById("canvas_result");
+	//display_image_from_json(json.value, canvas, img.width, img.height, 2);
+	display_image(img, canvas, 1);
 	
 	function image_to_json(image) {
 		var json_result = '{';
@@ -47,8 +48,68 @@
 		return(json_result);
 	}
 	
-	function display_image(json_data, width, height, orientation) {
-		var canvas=document.getElementById("canvas_result");
+	function display_image(image, canvas, orientation) {
+		var tempCanvas = document.createElement('canvas');
+		var tempContext = tempCanvas.getContext("2d");
+		tempContext.drawImage(image,0,0);
+		var realContext = canvas.getContext("2d");
+		var pixel_data, transparency;
+		
+		switch(orientation) {
+			case 0:
+				for(var i = 0; i < img.width; i++) {
+					for(var j = 0; j < img.height; j++) {
+						pixel_data = tempContext.getImageData(i, j, 1, 1).data;
+						transparency = pixel_data[3] / 255;
+						realContext.fillStyle="rgba("+pixel_data[0]+","+pixel_data[1]+","+pixel_data[2]+","+transparency+")";
+						realContext.fillRect(i,j,1,1);
+					}
+				}
+			break;
+			case 1:
+				for(var i = img.width-1; i > 0; i--) {
+					for(var j = 0; j < img.height; j++) {
+						pixel_data = tempContext.getImageData(i, j, 1, 1).data;
+						transparency = pixel_data[3] / 255;
+						realContext.fillStyle="rgba("+pixel_data[0]+","+pixel_data[1]+","+pixel_data[2]+","+transparency+")";
+						realContext.fillRect(img.width-1-i,j,1,1);
+					}
+				}
+			break;
+			case 2:
+				for(var i = 0; i < img.width; i++) {
+					for(var j = img.height-1; j > 0; j--) {
+						pixel_data = tempContext.getImageData(i, j, 1, 1).data;
+						transparency = pixel_data[3] / 255;
+						realContext.fillStyle="rgba("+pixel_data[0]+","+pixel_data[1]+","+pixel_data[2]+","+transparency+")";
+						realContext.fillRect(i,img.height-1-j,1,1);
+					}
+				}
+			break;
+			case 3:
+				for(var i = img.width-1; i > 0; i--) {
+					for(var j = img.height-1; j > 0; j--) {
+						pixel_data = tempContext.getImageData(i, j, 1, 1).data;
+						transparency = pixel_data[3] / 255;
+						realContext.fillStyle="rgba("+pixel_data[0]+","+pixel_data[1]+","+pixel_data[2]+","+transparency+")";
+						realContext.fillRect(img.width-1-i,img.height-1-j,1,1);
+					}
+				}
+			break;
+			default:
+				for(var i = 0; i < img.width; i++) {
+					for(var j = 0; j < img.height; j++) {
+						pixel_data = tempContext.getImageData(i, j, 1, 1).data;
+						transparency = pixel_data[3] / 255;
+						realContext.fillStyle="rgba("+pixel_data[0]+","+pixel_data[1]+","+pixel_data[2]+","+transparency+")";
+						realContext.fillRect(i,j,1,1);
+					}
+				}
+			break;
+		}
+	}
+	
+	function display_image_from_json(json_data, canvas, width, height, orientation) {
 		var ctx=canvas.getContext("2d");
 		var json_data_array = JSON.parse(json_data);
 		
